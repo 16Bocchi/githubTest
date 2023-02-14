125eb0c8eb6925456272b7926599039341ba9c90
16Bocchi <oce.bocchi@gmail.com>
2023-02-15 09:50:57 +1100

    Initial Commit

---
+++ githubTest/githubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubTestApp.swift
+//  githubTest
+//
+//  Created by Daniel Braithwaite on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct githubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
